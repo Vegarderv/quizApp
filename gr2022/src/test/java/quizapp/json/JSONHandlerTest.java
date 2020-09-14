b45cd26bd6de6d13d8b42561145e0fea9067be63
Malin Steine Døskeland <malinsd@stud.ntnu.no>
2020-09-14 13:16:32 +0000

    fixed json test

---
+++ gr2022/src/test/java/quizapp/json/JSONHandlerTest.java
@@ -0,0 +1,28 @@
+package quizapp.json;
+
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class JSONHandlerTest {
+	
+	static JSONHandler handler;
+	static HashMap<String, String> usernames = new HashMap<>();
+	HashMap<String, String> loadedUsernames;
+	
+	@BeforeAll
+	public static void setUp(){
+		handler = new JSONHandler("src/test/java/quizapp/json/JSONHandler.json");
+		usernames.put("gr2022", "gitlab");
+	}
+	
+	@Test
+	public void saveAndLoad() {
+		handler.writeToFile(usernames);
+		loadedUsernames = handler.loadFromFile();
+		assertEquals(usernames, loadedUsernames);
+	}
+}
