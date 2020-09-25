3eb415cd638f845fc9b6804db223902ef35fe5cf
Vegard Ervik <vegaer@stud.ntnu.no>
2020-09-25 09:09:41 +0000

    Wrote tests for JSONHandler (#24)

--- gr2022/src/main/java/quizapp/json/JSONHandler.java
+++ gr2022/src/main/java/quizapp/json/JSONHandler.java
@@ -60,44 +60,7 @@ public class JSONHandler {
     }
 
     
-    /*
-        Method for saving active user to file.
-        Use an empty string for reseting the user
-        */
-    public void saveActiveUser(String username){
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String javaObjectString = gson.toJson(username); // converts to json
-		try {
-
-			file = new FileWriter(path);
-			file.write(javaObjectString);
-
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-
-		} finally {
-
-			try {
-				file.flush();
-				file.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-    }
     
-    public String loadActiveUser(){
-        try {
-			FileReader reader = new FileReader(path);
-			String user = new Gson().fromJson(reader, new TypeToken<String>(){}.getType());
-			return user;
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
-    }
 
 	
 }
--- gr2022/src/test/java/quizapp/json/JSONHandlerTest.java
+++ gr2022/src/test/java/quizapp/json/JSONHandlerTest.java
@@ -1,10 +1,16 @@
 package quizapp.json;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import quizapp.core.User;
 import org.junit.jupiter.api.Test;
 
@@ -26,30 +32,38 @@ public class JSONHandlerTest {
         user2.setPassword("Stoica");
         usernames.add(user1);
         usernames.add(user2);
+        user1.addQuiz("testquiz123", 0.69);
         handler.writeToFile(usernames);
 	}
 	
 	@Test
-	public void FirstUsername() {
+	public void firstUsername() {
 		loadedUsernames = handler.loadFromFile();
 		assertEquals("Hallvard", loadedUsernames.get(0).getUsername());
     }
     
     @Test
-	public void FirstPassword() {
+	public void firstPassword() {
 		loadedUsernames = handler.loadFromFile();
 		assertEquals("Trætteberg", loadedUsernames.get(0).getPassword());
     }
     
     @Test
-	public void SecondUsername() {
+	public void secondUsername() {
 		loadedUsernames = handler.loadFromFile();
 		assertEquals("George", loadedUsernames.get(1).getUsername());
     }
     
     @Test
-	public void SecondPassword() {
+	public void secondPassword() {
 		loadedUsernames = handler.loadFromFile();
 		assertEquals("Stoica", loadedUsernames.get(1).getPassword());
-	}
+    }
+    
+    @Test
+    public void correctQuiz() {
+        loadedUsernames = handler.loadFromFile();
+        assertTrue(loadedUsernames.get(0).quizTaken("testquiz123"));
+    }
+	
 }
