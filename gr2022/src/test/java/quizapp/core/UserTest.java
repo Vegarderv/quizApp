d6c7b2e0f4010c8e6456205de8b2a15b6e32ece2
Vegard Ervik <vegaer@stud.ntnu.no>
2020-10-05 08:36:13 +0000

    Added @BeforeEach annontation to UserTest (#24)

--- gr2022/src/test/java/quizapp/core/UserTest.java
+++ gr2022/src/test/java/quizapp/core/UserTest.java
@@ -5,12 +5,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class UserTest{
     private User user = new User();
     
-    private void setUser(){
+    @BeforeEach
+    public void setUser(){
         //Using private method instead of @BeforeAll to avoid static method
 
         user.setUsername("Hallvard");
@@ -22,7 +24,6 @@ public class UserTest{
     public void correctUsernameAndPassword(){
         //Tests the getter methods
 
-        setUser();
         assertEquals(user.getUsername(), "Hallvard");
         assertEquals(user.getPassword(), "Trætteberg");
     }
@@ -30,8 +31,6 @@ public class UserTest{
     @Test
     public void correctMeanScore(){
         //Checks User.meanScore() method
-
-        setUser();
         assertEquals(user.meanScore(), 0.5);
     }
 
@@ -39,7 +38,6 @@ public class UserTest{
     public void hasTakenQuiz(){
         //Checks if User.quizTaken() works properly
 
-        setUser();
         assertTrue(user.quizTaken("quiz1"));
     }
 
@@ -47,7 +45,6 @@ public class UserTest{
     public void hasNotTakenQuiz(){
         //Checks if User.quizTaken() works properly
 
-        setUser();
         assertFalse(user.quizTaken("quiz3"));
     }
 
@@ -55,7 +52,6 @@ public class UserTest{
     public void getCorrectScore(){
         //Checks if User.getScore() works properly
 
-        setUser();
         assertEquals(1.0, user.getScore("quiz1"));
     }
 }
