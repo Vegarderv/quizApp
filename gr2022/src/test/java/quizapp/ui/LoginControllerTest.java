6ca87212a41965581a0794bc61660da426d7f554
Frida Fosli <fridafo@stud.ntnu.no>
2020-10-05 14:14:27 +0000

    added test to the signup button in the login page (#30)

--- gr2022/src/test/java/quizapp/ui/LoginControllerTest.java
+++ gr2022/src/test/java/quizapp/ui/LoginControllerTest.java
@@ -47,15 +47,20 @@ public class LoginControllerTest extends ApplicationTest {
 
      @Test
     public void checkWrongUsername() {
+        //checks that login page is at right stage
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
+        //assiging invalid values as password and username
         TextField usernameField = (TextField) stage.getScene().lookup("#username");
         usernameField.setText("Halva");
         TextField passwordField = (TextField) stage.getScene().lookup("#password");
         passwordField.setText("Trætteberg");
+        //clicks on log in button
         clickOn("#mainPageButton");
+        //checks that im still at login page
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
+        //checks that the error label shows the right error message
         Label error = (Label) stage.getScene().lookup("#errorMessage");
         assertEquals("Wrong username or password", error.getText());
     }
@@ -64,12 +69,24 @@ public class LoginControllerTest extends ApplicationTest {
     public void checkValidFields() {
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
+        //assiging valid values for the username and password field
         TextField usernameField = (TextField) stage.getScene().lookup("#username");
         usernameField.setText("Hallvard");
         TextField passwordField = (TextField) stage.getScene().lookup("#password");
         passwordField.setText("Trætteberg");
         clickOn("#mainPageButton");
+        //checks that scene is changed to the page menu after login button is pressed
         assertNull(stage.getScene().lookup("#mainPageButton"));
         assertNotNull(stage.getScene().lookup("#historyQuizButton"));
+    }
+
+    //need to check sign up button
+    @Test
+    public void signUpTest() {
+        assertNotNull(stage.getScene().lookup("#signUpButtonLoginPage"));
+        assertNull(stage.getScene().lookup("#signupButton"));
+        clickOn("#signUpButtonLoginPage");
+        assertNull(stage.getScene().lookup("#signUpButtonLoginPage"));   
+        assertNotNull(stage.getScene().lookup("#signupButton")); 
     }
 }
