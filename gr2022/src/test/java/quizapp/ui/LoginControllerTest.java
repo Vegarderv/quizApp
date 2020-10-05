b8d6f529c068f72f7180ec99dc1bb38ed4a52aba
Frida Fosli <fridafo@stud.ntnu.no>
2020-10-05 09:57:43 +0000

    fixing the last bugs (#30)

--- gr2022/src/test/java/quizapp/ui/LoginControllerTest.java
+++ gr2022/src/test/java/quizapp/ui/LoginControllerTest.java
@@ -1,77 +1,73 @@
 package quizapp.ui;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import quizapp.core.User;
-import quizapp.json.JSONHandler;
 
 public class LoginControllerTest extends ApplicationTest {
     
-    private static LoginController login;
-    private static JSONHandler handler;
-    private static List<User> users = new ArrayList<>();
     private Stage stage;
 
-
-    @BeforeAll
-    public void setUp(final Stage stage) throws Exception {
+    @Override
+    public void start(final Stage stage) throws Exception {
         final FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
         final Parent root = loader.load();
         stage.setScene(new Scene(root));
         stage.show();
         this.stage = stage;
-        login = new LoginController();
-        handler = new JSONHandler("src/main/resources/quizapp/json/JSONHandler.json");
-        User user = new User();
-        user.setUsername("Gløs");
-        user.setPassword("T-town");
-        users.add(user);
-        handler.writeToFile(users);
     }
 
      @Test
     public void checkWrongPassword() {
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
-        login.username.setText("Gløs");
-        login.password.setText("Heisann");
+        //assigns the textfields example values to test the logic
+        TextField usernameField = (TextField) stage.getScene().lookup("#username");
+        usernameField.setText("Hallvard");
+        TextField passwordField = (TextField) stage.getScene().lookup("#password");
+        passwordField.setText("Tretteberg");
         clickOn("#mainPageButton");
+        //expects the scene to stay at sign in page
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
-        assertTrue(login.errorMessage.getText().equals("Wrong username or password"));
+        //fetches the string of the label and checks if this is the same as the expected string
+        Label error = (Label) stage.getScene().lookup("#errorMessage");
+        assertEquals("Wrong username or password", error.getText());
     }
 
      @Test
     public void checkWrongUsername() {
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
-        login.username.setText("Gløsen");
-        login.password.setText("T-town");
+        TextField usernameField = (TextField) stage.getScene().lookup("#username");
+        usernameField.setText("Halvar");
+        TextField passwordField = (TextField) stage.getScene().lookup("#password");
+        passwordField.setText("Trætteberg");
         clickOn("#mainPageButton");
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
-        assertTrue(login.errorMessage.getText().equals("Wrong username or password"));
+        Label error = (Label) stage.getScene().lookup("#errorMessage");
+        assertEquals("Wrong username or password", error.getText());
     }
 
      @Test
     public void checkValidFields() {
         assertNull(stage.getScene().lookup("#historyQuizButton"));
         assertNotNull(stage.getScene().lookup("#mainPageButton"));
-        login.username.setText("Gløs");
-        login.password.setText("T-town");
+        TextField usernameField = (TextField) stage.getScene().lookup("#username");
+        usernameField.setText("Hallvard");
+        TextField passwordField = (TextField) stage.getScene().lookup("#password");
+        passwordField.setText("Trætteberg");
         clickOn("#mainPageButton");
         assertNull(stage.getScene().lookup("#mainPageButton"));
         assertNotNull(stage.getScene().lookup("#historyQuizButton"));
