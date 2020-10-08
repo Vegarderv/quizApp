9d98987b6e354ea3cfe4a8bc227a2e1cc2024659
Malin Steine Døskeland <malinsd@stud.ntnu.no>
2020-10-08 13:18:38 +0000

    Fixed some checkstyle violations (#34)

--- gr2022/core/src/main/java/quizapp/core/UsernameCheck.java
+++ gr2022/core/src/main/java/quizapp/core/UsernameCheck.java
@@ -4,19 +4,18 @@ import java.util.List;
 import quizapp.json.JsonHandler;
 
 public class UsernameCheck {
-  
+
   /**
-  * Function checks if username and password is valid.
-  */
+   * Function checks if username and password is valid.
+   */
   public boolean checkUsername(String username, String password) {
-    JsonHandler handler = new JsonHandler("/workspace/gr2022/gr2022/core/src/main/resources/quizapp/json/JSONHandler.json");
+    JsonHandler handler = new JsonHandler(
+        "/workspace/gr2022/gr2022/core/src/main/resources/quizapp/json/JSONHandler.json");
     List<User> userPasswords = handler.loadFromFile();
     if (!userPasswords.stream().anyMatch(a -> a.getUsername().equals(username))) {
       return false;
-    } else if (!userPasswords.stream()
-        .filter(p -> p.getUsername().equals(username))
-        .findFirst().get()
-        .getPassword().equals(password)) { //Chekcs if passwords match
+    } else if (!userPasswords.stream().filter(p -> p.getUsername().equals(username))
+        .findFirst().get().getPassword().equals(password)) { // Chekcs if passwords match
       return false;
     }
     return true;
