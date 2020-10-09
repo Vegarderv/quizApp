3b571fbd5bbeec1b254ed832a0805ce033dee61c
Therese Sigmundstad <thersi@stud.ntnu.no>
2020-10-09 12:45:50 +0000

    Edited ProfilePageControllerTest and fxml file.

--- gr2022/fxui/src/test/java/quizapp/ui/ProfilePageControllerTest.java
+++ gr2022/fxui/src/test/java/quizapp/ui/ProfilePageControllerTest.java
@@ -13,63 +13,55 @@ import quizapp.json.JsonHandler;
 import quizapp.json.UsernameHandler;
 import quizapp.core.User;
 
-public class ProfilePageControllerTest extends ApplicationTest {
-    private Stage stage;
-  
+public class ProfilePageControllerTest extends FxuiTest {
+  private Stage stage;
 
-    @Override
-    public void start(final Stage stage) throws Exception {
-        final FXMLLoader loader = new FXMLLoader(getClass().getResource("ProfilePage.fxml"));
-        final Parent root = loader.load();
-        stage.setScene(new Scene(root));
-        stage.show();
-        this.stage = stage;
-    }
+  @Override
+  public void start(final Stage stage) throws Exception {
+    final FXMLLoader loader = new FXMLLoader(getClass().getResource("ProfilePage.fxml"));
+    final Parent root = loader.load();
+    stage.setScene(new Scene(root));
+    stage.show();
+    this.stage = stage;
+  }
 
-    @Test
-    public void goToMainPageTest() {
-        assertNotNull(stage.getScene().lookup("#mainPageId")); 
-        assertNull(stage.getScene().lookup("#historyQuizButton"));
-         clickOn("#menubutton").clickOn("#mainPageId");
-        try {
-            Thread.sleep(400);
-        } catch (InterruptedException e) {
-        }
-        assertNotNull(stage.getScene().lookup("#historyQuizButton"));
-        assertNull(stage.getScene().lookup("#mainPageId"));
+  @Test
+  public void goToMainPageTest() {
+    assertNotNull(stage.getScene().lookup("#scoreId"));
+    assertNull(stage.getScene().lookup("#historyQuizButton"));
+    clickOnMenuItem("#menubutton", "menuPageId");
+    assertNotNull(stage.getScene().lookup("#historyQuizButton"));
+    assertNull(stage.getScene().lookup("#mainPageId"));
 
-    }
-    @Test
-    public void goToLogOut() {
-        assertNotNull(stage.getScene().lookup("#loginId")); 
-        assertNull(stage.getScene().lookup("#mainPageButton"));
-        clickOn("#menubutton").clickOn("#loginId");
-        try {
-            Thread.sleep(400);
-        } catch (InterruptedException e) {
-        }
-        assertNotNull(stage.getScene().lookup("#mainPageButton"));
-        assertNull(stage.getScene().lookup("#loginId"));
+  }
 
-    }
-    @Test
-public void correctUserNameTest(){
-        UsernameHandler userHandler = new UsernameHandler("src/main/resources/quizapp/json/activeUserTest.json");
-        FxAssert.verifyThat("#nameId", org.testfx.matcher.control.LabeledMatchers.hasText(userHandler.loadActiveUser()));
-        }
-        
-        @Test
-public void correctScoreTest(){
-  JsonHandler jsonHandler = new JsonHandler(
-        "src/main/resources/quizapp/json/JSONHandler.json");
+  @Test
+  public void goToLogOut() {
+    assertNotNull(stage.getScene().lookup("#scoreId"));
+    assertNull(stage.getScene().lookup("#mainPageButton"));
+    clickOnMenuItem("#menubutton", "loginId");
+    assertNotNull(stage.getScene().lookup("#mainPageButton"));
+    assertNull(stage.getScene().lookup("#loginId"));
+
+  }
+
+  @Test
+  public void correctUserNameTest() {
+    UsernameHandler userHandler = new UsernameHandler(
+        "/workspace/gr2022/gr2022/core/src/main/resources/quizapp/json/activeUserTest.json");
+    FxAssert.verifyThat("#nameId", org.testfx.matcher.control.LabeledMatchers.hasText(userHandler.loadActiveUser()));
+  }
+
+  @Test
+  public void correctScoreTest() {
+    JsonHandler jsonHandler = new JsonHandler(
+        "/workspace/gr2022/gr2022/core/src/main/resources/quizapp/json/JSONHandler.json");
     UsernameHandler usernameHandler = new UsernameHandler(
-        "src/main/resources/quizapp/json/activeUser.json");
-    User user=jsonHandler.loadFromFile().stream()
-        .filter(u-> u.getUsername()
-        .equals(usernameHandler.loadActiveUser()))
-        .findFirst().get();
+        "/workspace/gr2022/gr2022/core/src/main/resources/quizapp/json/activeUser.json");
+    User user = jsonHandler.loadFromFile().stream()
+        .filter(u -> u.getUsername().equals(usernameHandler.loadActiveUser())).findFirst().orElseThrow();
 
-  FxAssert.verifyThat("#scoreId", org.testfx.matcher.control.LabeledMatchers.hasText(user.meanScore().toString()));
+    FxAssert.verifyThat("#scoreId", org.testfx.matcher.control.LabeledMatchers.hasText(user.meanScore().toString()));
 
+  }
 }
-      }
