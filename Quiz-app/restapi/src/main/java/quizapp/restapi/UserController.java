4b2921f59a1d46a28e0b2bf0abf4b97feeb8e46b
Vegard Ervik <vegaer@stud.ntnu.no>
2020-10-30 17:44:49 +0000

    Added /update to UserController (#50)

--- Quiz-app/restapi/src/main/java/quizapp/restapi/UserController.java
+++ Quiz-app/restapi/src/main/java/quizapp/restapi/UserController.java
@@ -1,6 +1,8 @@
 package quizapp.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 import quizapp.core.User;
@@ -8,19 +10,25 @@ import quizapp.json.JsonHandler;
 
 import java.util.List;
 
-
 @RestController
 public class UserController {
 
-
   @GetMapping("/users")
-  public List<User> users(){
-    return new JsonHandler("/workspace/gr2022/Quiz-app/core/src/main/resources/quizapp/json/JSONHandler.json").loadFromFile();
+  public List<User> users() {
+    return new JsonHandler("/workspace/gr2022/Quiz-app/core/src/main/resources/quizapp/json/JSONHandler.json")
+        .loadFromFile();
   }
 
   @GetMapping("/user")
   public User getUser(@RequestParam String name) {
-    return new JsonHandler("/workspace/gr2022/Quiz-app/core/src/main/resources/quizapp/json/JSONHandler.json").loadUserFromString(
-        name);
+    return new JsonHandler("/workspace/gr2022/Quiz-app/core/src/main/resources/quizapp/json/JSONHandler.json")
+        .loadUserFromString(name);
   }
+
+  @PutMapping("/update")
+  public void updateUser(@RequestBody User user) {
+    new JsonHandler("/workspace/gr2022/Quiz-app/core/src/main/resources/quizapp/json/JSONHandler.json")
+        .updateUser(user);
+  }
+
 }
