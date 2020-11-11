80210eb03301b09b7250c5a870bd1e872d70254f
Malin Steine Døskeland <malinsd@stud.ntnu.no>
2020-11-11 18:15:53 +0000

    Fixed tests for making new quiz (#61)

--- Quiz-app/fxui/src/test/java/quizapp/ui/AddQuizTest.java
+++ Quiz-app/fxui/src/test/java/quizapp/ui/AddQuizTest.java
@@ -4,22 +4,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import quizapp.core.User;
 import quizapp.core.Quiz;
-import org.testfx.api.FxAssert;
-import org.testfx.matcher.base.NodeMatchers;
+import javafx.scene.control.MenuButton;
 
 import java.awt.*;
-import java.beans.Transient;
 import javafx.scene.control.TextField;
-import static org.testfx.api.FxAssert.verifyThat;
-import org.testfx.api.FxAssert;
 
 
 import org.junit.jupiter.api.Order;
@@ -31,13 +25,14 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 public class AddQuizTest extends FxuiTest {
 
   private Stage stage;
-  private DirectUserAccess directUserAccess;
   private DirectQuizAccess directQuizAccess;
+  private UserAccess directUserAccess;
 
 
   @Override
   public void start(final Stage stage) throws Exception {
     directQuizAccess = new DirectQuizAccess();
+    directUserAccess = new DirectUserAccess();
     final FXMLLoader loader = new FXMLLoader(getClass().getResource("AddQuiz.fxml"));
     final Parent root = loader.load();
     stage.setScene(new Scene(root));
@@ -51,6 +46,80 @@ public class AddQuizTest extends FxuiTest {
   public void MakeAQuizTest() throws AWTException {
     //Deletes quiz if it already exists
     directQuizAccess.deleteQuiz("Color-quiz");
+    fillInQuiz();
+    Quiz quiz = directQuizAccess.getQuiz("Color-quiz");
+    assertNotNull(quiz);
+    assertEquals("Color quiz", quiz.getName());
+    assertEquals(1, quiz.getQuestion(0).getCorrectAlternative());
+    assertEquals("Green", quiz.getQuestion(0).getAlternative(1));
+    assertEquals("What color do you get if you mix red and yellow?", quiz.getQuestion(1).getQuestion());
+    // Checks that the scene has changed to main menu
+    assertNull(stage.getScene().lookup("#mainMenu"));
+    assertNotNull(stage.getScene().lookup("#menuButton"));
+  }
+
+  @Test
+  @Order(2)
+  public void submitUncompleteQuizTest() throws AWTException {
+    Robot r = new Robot();
+    findTextField("#title").setText("Test quiz");
+    findTextField("#q0").setText("Is this a test?");
+    findTextField("#q0an0").setText("Yes");
+    findTextField("#q0an1").setText("No");
+    findTextField("#q0an2").setText("Purple");
+    findTextField("#q0an3").setText("Red");
+    clickOnButton("#q0a0");
+    r.mouseWheel(25);
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+    }
+    clickOnButton("#submit");
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+    }
+    String text = ((Label) stage.getScene().lookup("#scoreLabel")).getText();
+    assertEquals("Invalid Quiz. Check that all fields are filled and correct answers are chosen", text);
+  }
+
+  @Test
+  @Order(3)
+  public void makeQuizWithInvalidName() throws AWTException {
+    fillInQuiz();
+    String text = ((Label) stage.getScene().lookup("#scoreLabel")).getText();
+    assertEquals("Invalid Quizname. The title must be unique, there is already a quiz named Color quiz", text);
+  }
+
+
+
+  @Test
+  @Order(4)    
+  public void goToMainMenuTest() {
+    // Checks that we on add quiz page
+    assertNotNull(stage.getScene().lookup("#mainMenu"));
+    assertNull(stage.getScene().lookup("#menuButton"));
+    // Changes Scene to Main Menu
+    clickOnButton("#mainMenu");
+    // Checks that we are on the Main page scene
+    assertNull(stage.getScene().lookup("#mainMenu"));
+    assertNotNull(stage.getScene().lookup("#menuButton"));
+  }
+
+
+  @Test
+  @Order(5)    
+  public void checkCorrectUserDisplayed() {
+    // Checks active user and makes sure it matches username displayed on menu button
+    String activeUser = ((MenuButton)stage.getScene().lookup("#userMenu")).getText();
+    assertEquals(directUserAccess.getActiveUser().getUsername(), activeUser);
+  }
+    
+  private TextField findTextField(String node) {
+    return (TextField) stage.getScene().lookup(node);
+  }
+
+  private void fillInQuiz() throws AWTException {
     // Fills in everything needed to make a quiz
     Robot r = new Robot();
     findTextField("#title").setText("Color quiz");
@@ -93,58 +162,6 @@ public class AddQuizTest extends FxuiTest {
     } catch (InterruptedException e) {
     }
     clickOnButton("#submit");
-    Quiz quiz = directQuizAccess.getQuiz("Color-quiz");
-    assertNotNull(quiz);
-    assertEquals("Color quiz", quiz.getName());
-    assertEquals(1, quiz.getQuestion(0).getCorrectAlternative());
-    assertEquals("Green", quiz.getQuestion(0).getAlternative(1));
-    assertEquals("What color do you get if you mix red and yellow?", quiz.getQuestion(1).getQuestion());
-    // Checks that the scene has changed to main menu
-    assertNull(stage.getScene().lookup("#mainMenu"));
-    assertNotNull(stage.getScene().lookup("#menuButton"));
-  }
-
-  @Test
-  @Order(2)
-  public void submitUncompleteQuizTest() throws AWTException {
-    Robot r = new Robot();
-    findTextField("#title").setText("Test quiz");
-    findTextField("#q0").setText("Is this a test?");
-    findTextField("#q0an0").setText("Yes");
-    findTextField("#q0an1").setText("No");
-    findTextField("#q0an2").setText("Purple");
-    findTextField("#q0an3").setText("Red");
-    clickOnButton("#q0a0");
-    r.mouseWheel(25);
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-    }
-    clickOnButton("#submit");
-    try {
-      Thread.sleep(1000);
-    } catch (InterruptedException e) {
-    }
-    String text = ((Label) stage.getScene().lookup("#scoreLabel")).getText();
-    assertEquals("Invalid Quiz. Check that all fields are filled and correct answers are chosen", text);
-  }
-
-
-  @Test
-  @Order(3)    
-  public void goToMainMenuTest() {
-    // Checks that we on add quiz page
-    assertNotNull(stage.getScene().lookup("#mainMenu"));
-    assertNull(stage.getScene().lookup("#menuButton"));
-    // Changes Scene to Main Menu
-    clickOnButton("#mainMenu");
-    // Checks that we are on the Main page scene
-    assertNull(stage.getScene().lookup("#mainMenu"));
-    assertNotNull(stage.getScene().lookup("#menuButton"));
-  }
-    
-  private TextField findTextField(String node) {
-    return (TextField) stage.getScene().lookup(node);
   }
 
 
