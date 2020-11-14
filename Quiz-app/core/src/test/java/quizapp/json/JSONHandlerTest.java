af6981ddd7220e95944d3fb9e3bf4f7175285339
Malin Steine Døskeland <malinsd@stud.ntnu.no>
2020-11-14 13:08:35 +0000

    Updated tests (#67)

--- Quiz-app/core/src/test/java/quizapp/json/JSONHandlerTest.java
+++ Quiz-app/core/src/test/java/quizapp/json/JSONHandlerTest.java
@@ -1,7 +1,6 @@
 package quizapp.json;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import quizapp.core.User;
